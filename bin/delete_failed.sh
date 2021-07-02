401f6275dd9797d4befb03971584210105d55bb1
Stu Franks <stu@stuts.uk>
2021-07-02 17:05:04 +0100

    Add deletion script

---
+++ bin/delete_failed.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+# Gather config
+source $DIR/config.sh
+
+echo "WARNING: This script will delete the VM and associated disks as specified in config.sh"
+echo
+read -p "Remove $VM_NAME and $VM_DISK? [y/N] " answer
+
+case $answer in
+    y|Y)
+        echo "Deleting..."
+        virsh destroy $VM_NAME
+        virsh undefine $VM_NAME
+        rm -f $VM_DISK
+        ;;
+    *)
+        echo "Not deleting anything."
+        ;;
+esac
+
