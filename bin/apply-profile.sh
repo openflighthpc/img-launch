c5c4a2d1252b49ba38f9b6ba3a062d97dfe79902
Stu Franks <stu@stuts.uk>
2021-07-02 17:17:29 +0100

    Support for VMs without guest utils

--- bin/apply-profile.sh
+++ bin/apply-profile.sh
@@ -9,7 +9,7 @@ source $DIR/config.sh
 echo "==== APPLYING PROFILE CONFIGURATION ===="
 
 EXT_MAC="$(virsh domiflist $VM_NAME |grep $EXT_BRIDGE |awk '{print $5}')"
-IP="$(virsh  domifaddr --source agent $VM_NAME |grep $EXT_MAC |awk '{print $4}' |sed 's,/.*,,g')"
+IP="$(arp -e |grep $EXT_MAC |awk '{print $1}')"
 
 scp $DIR/scripts/$PROFILE_SCRIPT root@$IP:/tmp/$PROFILE_SCRIPT
 ssh root@$IP "bash /tmp/$PROFILE_SCRIPT"
