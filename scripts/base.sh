5b40ba2a68c973c9649bc9aef82937276e6f247d
Stu Franks <stu@stuts.uk>
2021-07-09 14:23:01 +0100

    Add base profile script to configure networking

---
+++ scripts/base.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+#
+# Base Script for All Systems
+#
+
+yum -y install firewalld
+systemctl enable firewalld
+systemctl start firewalld
+
+#
+# Network setup
+#
+
+cat << EOF >> /etc/hosts
+# $VM_NAME
+$SITE_IP    $VM_NAME.$DOMAIN $VM_NAME.$SITE_NAME $VM_NAME
+$PRI_IP    $VM_NAME.pri.$CLUSTER_DOMAIN $VM_NAME.pri.$CLUSTER_NAME.$SITE_NAME $VM_NAME.pri.$CLUSTER_NAME
+$MGT_IP    $VM_NAME.mgt.$CLUSTER_DOMAIN $VM_NAME.mgt.$CLUSTER_NAME.$SITE_NAME $VM_NAME.mgt.$CLUSTER_NAME
+EOF
+
+
+if [[ ! -z "$EXT_BRIDGE" ]] ; then
+
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$EXT_IFACE
+TYPE=Ethernet
+BOOTPROTO=dhcp
+DEFROUTE=yes
+PEERDNS=yes
+PEERROUTES=yes
+NAME=$EXT_IFACE
+DEVICE=$EXT_IFACE
+ONBOOT=yes
+ZONE=external
+EOF
+
+    firewall-offline-cmd --add-masquerade --zone public --permanent
+    firewall-offline-cmd --add-interface $EXT_IFACE --zone public --permanent
+fi
+
+if [[ ! -z "$SITE_BRIDGE" ]] ; then
+
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$SITE_IFACE
+TYPE=Ethernet
+BOOTPROTO=none
+DEFROUTE=no
+PEERDNS=no
+PEERROUTES=no
+NAME=$SITE_IFACE
+DEVICE=$SITE_IFACE
+ONBOOT=yes
+IPADDR=$SITE_IP
+NETMASK=255.255.0.0
+ZONE=site
+EOF
+
+    firewall-offline-cmd --new-zone site --permanent
+    firewall-offline-cmd --set-target=ACCEPT --zone site --permanent
+    firewall-offline-cmd --zone site --add-interface $SITE_IFACE --permanent
+fi
+
+
+if [[ ! -z "$PRI_BRIDGE" ]] ; then
+
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$PRI_IFACE
+TYPE=Ethernet
+BOOTPROTO=none
+DEFROUTE=no
+PEERDNS=no
+PEERROUTES=no
+NAME=$PRI_IFACE
+DEVICE=$PRI_IFACE
+ONBOOT=yes
+IPADDR=$PRI_IP
+NETMASK=255.255.0.0
+ZONE=cluster1
+EOF
+
+    firewall-offline-cmd --new-zone cluster1 --permanent
+    firewall-offline-cmd --set-target=ACCEPT --zone cluster1 --permanent
+    firewall-offline-cmd --zone cluster1 --add-interface $PRI_IFACE --permanent
+
+fi
+
+if [[ ! -z "$MGT_BRIDGE" ]] ; then
+
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$MGT_IFACE
+TYPE=Ethernet
+BOOTPROTO=none
+DEFROUTE=no
+PEERDNS=no
+PEERROUTES=no
+NAME=$MGT_IFACE
+DEVICE=$MGT_IFACE
+ONBOOT=yes
+IPADDR=$MGT_IP
+NETMASK=255.255.0.0
+ZONE=cluster1mgt
+EOF
+
+    firewall-offline-cmd --new-zone cluster1mgt --permanent
+    firewall-offline-cmd --set-target=ACCEPT --zone cluster1mgt --permanent
+    firewall-offline-cmd --zone cluster1mgt --add-interface $MGT_IFACE --permanent
+fi
+
+firewall-cmd --reload
+
+#
+# Disable Cloud Init
+#
+
+#disable cloudinit on next run
+touch /etc/cloud/cloud-init.disabled
+
+systemctl disable cloud-init
+systemctl disable cloud-config
+systemctl disable cloud-final
+systemctl disable cloud-init-local
+
+#
+# System Config
+#
+
+cat << EOF > /etc/profile.d/flightcenter.sh
+#Custom PS1 with client name
+[ -f /etc/flightcentersupported ] && c=32 || c=31
+if [ "\$PS1" ]; then
+  PS1="[\u@\h\[\e[1;\${c}m\][$VM_NAME-$SITE_NAME]\[\e[0m\] \W]\\$ "
+fi
+EOF
+touch /etc/flightcentersupported
+
+sed -i -e 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+echo 'ZONE="Europe/London"' > /etc/sysconfig/clock
+ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime
+
+
+yum -y update
--- scripts/director.sh
+++ scripts/director.sh
@@ -3,8 +3,4 @@
 # Profile Script for a Director VM
 #
 
-cat << 'EOF' > /etc/profile.d/director-prompt.sh
-if [ "$PS1" ]; then
-  PS1="[\u@\h\[\e[1;34m\] [Director]\[\e[0m\] \W]\\$ "
-fi
-EOF
+
