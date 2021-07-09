f0f61b1de631c4d2804b844bf95603a5bcf7e60a
Stu Franks <stu@stuts.uk>
2021-07-09 14:22:41 +0100

    Restructure profile application

--- bin/apply-profile.sh
+++ bin/apply-profile.sh
@@ -11,7 +11,17 @@ echo "==== APPLYING PROFILE CONFIGURATION ===="
 EXT_MAC="$(virsh domiflist $VM_NAME |grep $EXT_BRIDGE |awk '{print $5}')"
 IP="$(arp -e |grep $EXT_MAC |awk '{print $1}')"
 
-scp -o StrictHostKeyChecking=no -o PasswordAuthentication=no $DIR/scripts/$PROFILE_SCRIPT root@$IP:/tmp/$PROFILE_SCRIPT
-ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no root@$IP "bash /tmp/$PROFILE_SCRIPT"
+SSH_ARGS='-o StrictHostKeyChecking=no -o PasswordAuthentication=no'
+
+SCRIPT="$(cat << EOF
+$(cat $DIR/config.sh)
+$(echo "---- Applying Base Configuration ----")
+$(cat $DIR/scripts/base.sh)
+$(echo "---- Applying Profile Configuration ($PROFILE_SCRIPT) ----")
+$(cat $DIR/scripts/$PROFILE_SCRIPT)
+EOF
+)"
+
+ssh $SSH_ARGS root@$IP "echo '$SCRIPT' > /tmp/flightscript.sh ; bash /tmp/flightscript.sh"
 
 echo "==== FINISHED APPLYING PROFILE CONFIGURATION TO IP: $IP ===="
