c254d1ef8c62b0b066cf7e59084be59f286e4a7b
Stu Franks <stu@stuts.uk>
2021-07-09 14:30:38 +0100

    Move gateway setup to director script

--- scripts/director.sh
+++ scripts/director.sh
@@ -3,4 +3,8 @@
 # Profile Script for a Director VM
 #
 
-
+#
+# Setup as Gateway
+#
+firewall-offline-cmd --add-masquerade --zone public --permanent
+firewall-offline-cmd --add-interface $EXT_IFACE --zone public --permanent
