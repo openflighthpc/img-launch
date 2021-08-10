90ad5b6c1c0c3431b975102de1d428169194e313
Stu Franks <stu@stuts.uk>
2021-08-10 17:18:26 +0100

    Simple DNS forwarding for gateway

--- scripts/gateway.sh
+++ scripts/gateway.sh
@@ -10,6 +10,14 @@ firewall-offline-cmd --add-masquerade --zone public
 firewall-offline-cmd --add-interface $EXT_IFACE --zone public
 
 #
+# DNS Forwarding
+#
+yum install -y dnsmasq
+
+systemctl start dnsmasq
+systemctl enable dnsmasq
+
+#
 # Client Script
 #
 yum -y install httpd
@@ -36,8 +44,10 @@ IFACE="eth0"
 NET="\$(grep "^IPADDR" /etc/sysconfig/network-scripts/ifcfg-\$IFACE |sed "s/IPADDR=//g" |cut -d "." -f1,2)"
 GW_IP_END="$(echo "$SITE_IP" |cut -d '.' -f3,4)"
 grep -q GATEWAY /etc/sysconfig/network-scripts/ifcfg-\$IFACE && sed -i "s/GATEWAY=.*/GATEWAY=\$NET.\$GW_IP_END/g" /etc/sysconfig/network-scripts/ifcfg-\$IFACE || echo "GATEWAY=\$NET.\$GW_IP_END" >> /etc/sysconfig/network-scripts/ifcfg-\$IFACE
+grep -q DNS1 /etc/sysconfig/network-scripts/ifcfg-\$IFACE && sed -i "s/DNS1=.*/DNS1=\$NET.\$GW_IP_END/g" /etc/sysconfig/network-scripts/ifcfg-\$IFACE || echo "DNS1=\$NET.\$GW_IP_END" >> /etc/sysconfig/network-scripts/ifcfg-\$IFACE
 sed -i "s/DEFROUTE=.*/DEFROUTE=yes/g" /etc/sysconfig/network-scripts/ifcfg-\$IFACE
 ip route replace default via \$NET.\$GW_IP_END dev \$IFACE
+echo "nameserver \$NET.\$GW_IP_END" >> /etc/resolv.conf
 EOF
 
 systemctl enable httpd
